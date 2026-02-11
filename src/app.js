4e134bc954588455905d21f7aac479d4a16ac4f6
Mankirat <mankirat2601@gmail.com>
2026-02-10 21:20:26 -0800

    Add multer for file upload

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+
 const app = express();
 
 app.use(
@@ -12,6 +14,8 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
+
+app.use(cookieParser());
 
 //import routes
 import healthCheckRoute from "./routes/healthcheck.routes.js";
