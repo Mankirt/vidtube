22e180933889f3e5f6420e2a50d51c8508d95647
Mankirat <mankirat2601@gmail.com>
2026-02-11 21:32:14 -0800

    Correct generateAccessAndRefreshTokens() location

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -2,22 +2,6 @@ import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-const generateAccessAndRefreshTokens = async (userId) => {
-    try {
-        const user = await User.findById(userId)
-        if (!user) {
-            throw new ApiError(404, "User not found")
-        }
-        const accessToken = user.generateAccessToken()
-        const refreshToken = user.generateRefreshToken()
-
-        user.refreshToken = refreshToken
-        await user.save({ validateBeforeSave: false })
-        return { accessToken, refreshToken }
-    } catch (error) {
-        throw new ApiError(500, "Failed to generate access and refresh tokens")
-    }
-}
 
 const userSchema = new Schema({
     username: {
