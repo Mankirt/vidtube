34e11938592807590e0a13bf07dbe4e3995a25aa
Mankirat <mankirat2601@gmail.com>
2026-02-12 20:08:08 -0800

    Add logic to update user cover image

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -262,5 +262,26 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
 }
 )
 
+const updateUserCoverImage = asyncHandler(async (req, res) => {
+    const coverImageLocalPath = req.file?.path
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover image is required")
+    }
+    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
+    if (!coverImage.url) {
+        throw new ApiError(500, "Failed to upload cover image")
+    }
+    const user = await User.findByIdAndUpdate(req.user._id, {
+        $set: {
+            coverImage: coverImage.url
+        }
+    },
+        { new: true }
+    ).select("-password -refreshToken")
 
-export { registerUser, loginUser, refreshAccessToken, logoutUser, changeCurrentPassword, getCurrentUser, updateUserAvatar };
+    return res.status(200).json(new ApiResponse(200, user, "Cover image updated successfully"))
+}
+)
+
+
+export { registerUser, loginUser, refreshAccessToken, logoutUser, changeCurrentPassword, getCurrentUser, updateUserAvatar, updateUserCoverImage };
