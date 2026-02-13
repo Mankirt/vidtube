dffde686c7454a56500110502bad11837eb2493a
Mankirat <mankirat2601@gmail.com>
2026-02-12 20:03:49 -0800

    Add logic to update user

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -223,5 +223,23 @@ const getCurrentUser = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, req.user, "Current user fetched successfully"))
 })
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullname, email } = req.body
+    if (!fullname || !email) {
+        throw new ApiError(400, "Fullname and email are required")
+    }
+    const user = await User.findByIdAndUpdate(req.user._id, {
+        $set: {
+            fullname,
+            email
+        }
+    },
+        { new: true }
+    ).select("-password -refreshToken")
+
+    return res.status(200).json(new ApiResponse(200, user, "Account details updated successfully"))
+
+})
+
 
 export { registerUser, loginUser, refreshAccessToken, logoutUser, changeCurrentPassword, getCurrentUser };
